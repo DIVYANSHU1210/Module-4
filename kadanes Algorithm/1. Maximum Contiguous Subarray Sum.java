fd70a3c7bc1804ba31434eab5141a2a8f829d5aa
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-01-14 15:07:46 +0530

    Create 1. Maximum Contiguous Subarray Sum.java

---
+++ kadanes Algorithm/1. Maximum Contiguous Subarray Sum.java
@@ -0,0 +1,28 @@
+// https://course.acciojob.com/idle?question=4265d603-e34e-4d86-84e8-0ce3c938c197
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = input.nextInt();
+        }
+        Solution ob =new Solution();
+        long ans = ob.maxSubarraySum(a,n);
+        System.out.println(ans);
+    }
+}
+class Solution{
+    public long maxSubarraySum(int a[],int n){
+        int sum = 0;
+		long maxsum = Integer.MIN_VALUE;
+		for(int i = 0; i<n; i++){
+			sum += a[i];
+			maxsum = Math.max(maxsum, sum);
+			if(sum < 0)sum = 0;
+		}
+		return maxsum;
+    }
+}
